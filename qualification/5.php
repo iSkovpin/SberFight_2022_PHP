a58526c0b24bb0b3daafe11867735ff4b39f847b
Ivan Skovpin <ivanskovpin@gmail.com>
2022-02-23 18:43:13 +0700

    * descriptions fix

--- qualification/5.php
+++ qualification/5.php
@@ -1,14 +1,30 @@
 <?php
 
 /*
- * Дан массив чисел и массив арифметических операций (равной длины).
- * Числа всегда перебираются в изначально заданной последовательности.
- * Математические операции могут следовать в любой последовательности и применяются к следующему числу в массиве.
- * Начальное значение перебора равно 0.
- * Найти максимальный результат такого перебора.
+    На вход подаётся массив чисел numb. Вам нужно выполнить арифметические действия, которые находятся в строковом массиве arith. В массиве arith находятся знаки: "+", "-", "//" - целочисленное деление, "*". Они означают соответствующие арифметические действия.
+    Вы можете использовать знаки в любом порядке, но порядок чисел в numb должен оставаться неизменным - первое число равно всегда нулю, далее numb[0], numb[1], ... Количество знаков всегда равно количеству чисел в массиве. Операции выполняются последовательно, без учета приоритета операций. Будьте внимательнее в операциях с 0//... - они не приводят ни к чему хорошему, игнорируйте их.
+    Получите наибольшее возможное число.
+
+    Ввод:
+
+        numb - массив чисел, 1<length(numb)<=10, 0<=numb[i]<=25
+        arith - массив возможных операций, arith[i]={"+" | "-" | "//" | "*"}
+
+    Вывод:
+
+        Integer - итоговое наибольшее число, которое получилось в результате всех арифметических действий
+
+    Пример:
+
+    numb = [3, 4]
+    arith = ["+", "-"]
+    getResult(numb, arith) = 1
+
+    Сначала выгодно из числа 0 вычесть 3 (получается -3), потом прибавить 4.
+    Ответ: 1.
  */
 
-function getResult(array $numb, array $arith): float
+function getResult(array $numb, array $arith): int
 {
     $arithPermutations = [];
     getUniquePermutations($arith, $arithPermutations);
@@ -21,13 +37,15 @@ function getResult(array $numb, array $arith): float
             continue;
         }
 
+        echo implode(', ' , $permutation) . ' |= ' , $permResult . "\n";
+
         if ($result === null || $result < $permResult) {
 //            echo implode(', ' , $permutation) . ' |= ' , $permResult . "\n";
             $result = $permResult;
         }
     }
 
-    return $result;
+    return (int) $result;
 }
 
 function getPermResult(array $numbers, array $arithOperations): float
@@ -47,7 +65,7 @@ function getPermResult(array $numbers, array $arithOperations): float
             if ($number === 0) {
                 throw new Exception('Division by zero');
             }
-            $result /= $number;
+            $result = intdiv($result, $number);
         } elseif ($operation === '*') {
             $result *= $number;
         }
@@ -73,9 +91,10 @@ function getUniquePermutations($inputArr, &$returnArr = [], $processedArr = [])
     }
 }
 
-//echo getResult([1, 2, 3, 4, 5, 6, 7, 8, 9, 9], ['+', '-', '-', '*', '//', '+', '-', '-', '*', '//']);
-//echo getResult([10, 2, 3, 4, 5, 6, 7, 8, 9, 9], ['+', '-', '+', '-', '+', '-', '+', '-', '*', '//',]);
-//echo getResult([1, 2, 3, 4], ['+', '-', '//', '*']);
-//echo getResult([3, 4, 12, 0, 1, 5, 25, 4, 5], ['+', '-', '+', '//', '//', '*', '*', '-', '-']);
-//echo getResult([1, 4, 2], ['+', '-', '*']);
-//echo getResult([3, 4], ['+', '-']);
+//echo getResult([1, 2, 3, 4, 5, 6, 7, 8, 9, 9], ['+', '-', '-', '*', '//', '+', '-', '-', '*', '//']) . "\n";
+//echo getResult([10, 2, 3, 4, 5, 6, 7, 8, 9, 9], ['+', '-', '+', '-', '+', '-', '+', '-', '*', '//',]) . "\n";
+//echo getResult([1, 2, 3, 4], ['+', '-', '//', '*']) . "\n";
+//echo getResult([3, 4, 12, 0, 1, 5, 25, 4, 5], ['+', '-', '+', '//', '//', '*', '*', '-', '-']) . "\n";
+//echo getResult([1], ['-']) . "\n";
+//echo getResult([1, 4, 2], ['-', '+', '*']) . "\n";
+//echo getResult([3, 4], ['+', '-']) . "\n";
